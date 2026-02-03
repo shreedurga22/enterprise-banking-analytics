c737940dfb589e0bed72813718427c3029004f61
Shree Durga G S <shreedurga114@gmail.com>
2026-02-03 11:40:24 +0530

    Create staging_transactions table for transaction data

---
+++ sql/01_staging_table.sql
@@ -0,0 +1,8 @@
+CREATE TABLE staging_transactions (
+    account_no VARCHAR(50),
+    txn_date VARCHAR(20),
+    value_date VARCHAR(20),
+    withdrawal_amt VARCHAR(20),
+    deposit_amt VARCHAR(20),
+    balance_amt VARCHAR(20)
+);
